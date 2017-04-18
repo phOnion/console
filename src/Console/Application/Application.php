778c32c87ddaf612eafb101627d9078fbc50b58c
Dimitar Dimitrov <daghostman.dd@gmail.com>
2017-04-19 00:49:19 +0300

    Improved overall --help handling

--- src/Console/Application/Application.php
+++ src/Console/Application/Application.php
@@ -21,35 +21,24 @@ class Application
             return 1;
         }
 
-        if (count($argv) === 1 && in_array('--help', $argv, true)) {
+        if (count($argv) === 1 && $argv[0] === '--help') {
             $console->writeLine('HELP', 'white');
             $console->writeLine('');
             foreach ($this->router->getAvailableCommands() as $command) {
-                $meta = $this->router->getCommandData($command);
-                $console->write("COMMAND \t", 'white');
-                $console->writeLine($command, 'bold-yellow');
-                $console->writeLine("DESCRIPTION", 'white');
-                $console->writeLine("\t" . $meta['description'], 'dark-gray');
-                if (!empty($meta['flags'])) {
-                    $console->writeLine("FLAGS", 'white');
-                    foreach ($meta['flags'] as $flag => $description) {
-                        $console->writeLine("\t-$flag", 'dark-gray');
-                        $console->writeLine("\t    " . $description, 'dark-gray');
-
-                    }
-                }
-                if (!empty($meta['parameters'])) {
-                    $console->writeLine("PARAMETERS", 'white');
-                    foreach ($meta['parameters'] as $argument => $description) {
-                        $console->writeLine("\t--$argument", 'dark-gray');
-                        $console->writeLine("\t    " . $description, 'dark-gray');
-                        $console->writeLine('');
-                    }
-                }
-                $console->writeLine(PHP_EOL);
+                $this->displayHelpInfo($console, $command);
             }
 
             return 0;
+        }
+
+        if (count($argv) === 2 && $argv[1] === '--help') {
+            if (in_array($argv[0], $this->router->getAvailableCommands(), true)) {
+                $console->writeLine('HELP', 'white');
+                $console->writeLine('');
+                $this->displayHelpInfo($console, $argv[0]);
+
+                return 0;
+            }
         }
 
         list($command, $arguments)=$this->router->match((string) $argv[0], $argv);
@@ -58,5 +47,31 @@ class Application
         }
 
         return $command->trigger($console);
+    }
+
+    private function displayHelpInfo(ConsoleInterface $console, string $command)
+    {
+        $meta = $this->router->getCommandData($command);
+        $console->write("COMMAND \t", 'white');
+        $console->writeLine($command, 'bold-yellow');
+        $console->writeLine("DESCRIPTION", 'white');
+        $console->writeLine("\t" . $meta['description'], 'dark-gray');
+        if (!empty($meta['flags'])) {
+            $console->writeLine("FLAGS", 'white');
+            foreach ($meta['flags'] as $flag => $description) {
+                $console->writeLine("\t-$flag", 'dark-gray');
+                $console->writeLine("\t    " . $description, 'dark-gray');
+
+            }
+        }
+        if (!empty($meta['parameters'])) {
+            $console->writeLine("PARAMETERS", 'white');
+            foreach ($meta['parameters'] as $argument => $description) {
+                $console->writeLine("\t--$argument", 'dark-gray');
+                $console->writeLine("\t    " . $description, 'dark-gray');
+                $console->writeLine('');
+            }
+        }
+        $console->writeLine(PHP_EOL);
     }
 }
