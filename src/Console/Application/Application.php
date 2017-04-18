936b2429b136d5eb478e90fd1943d9e0d3be4f91
Dimitar Dimitrov <daghostman.dd@gmail.com>
2017-04-18 21:59:43 +0300

    Added command handling and general '--help' handler

--- src/Console/Application/Application.php
+++ src/Console/Application/Application.php
@@ -17,10 +17,40 @@ class Application
     {
         if (!isset($argv[0])) {
             $console->writeLine('No command provided', 'red');
+            $console->writeLine('Try with --help to see list of available commands', 'cyan');
             return 1;
         }
-        list($command, $arguments)=$this->router->match((string) $argv[0]);
 
-        return 0;
+        if (count($argv) === 1 && in_array('--help', $argv, true)) {
+            foreach ($this->router->getAvailableCommands() as $command) {
+                $meta = $this->router->getCommandData($command);
+                $console->write("\tCommand: \t", 'yellow');
+                $console->writeLine($command, 'bold-green');
+                $console->write("\tDescription: \t", 'yellow');
+                $console->writeLine($meta['description'], 'italic-cyan');
+                $console->writeLine('');
+                $console->writeLine("\tFlags:", 'yellow');
+                foreach ($meta['flags'] as $flag => $description) {
+                    $console->write("\t\t-$flag", 'bold-green');
+                    $console->writeLine("\t\t" . $description, 'cyan');
+                    $console->writeLine('');
+                }
+                $console->writeLine("\tParameters:", 'yellow');
+                foreach ($meta['arguments'] as $argument => $description) {
+                    $console->write("\t\t--$argument", 'bold-green');
+                    $console->writeLine("\t" . $description, 'cyan');
+                    $console->writeLine('');
+                }
+            }
+
+            return 0;
+        }
+
+        list($command, $arguments)=$this->router->match((string) $argv[0], $argv);
+        foreach ($arguments as $name => $value) {
+            $console = $console->withArgument($name, $value);
+        }
+
+        return $command->trigger($console);
     }
 }
