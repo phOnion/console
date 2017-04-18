bfa763763bba7819a2dc746a2e77d74daf18dbb3
Dimitar Dimitrov <daghostman.dd@gmail.com>
2017-04-18 21:56:34 +0300

    Add interface for argument parsing

---
+++ src/Framework/Console/Interfaces/ArgumentParserInterface.php
@@ -0,0 +1,8 @@
+<?php declare(strict_types=1);
+
+namespace Onion\Framework\Console\Interfaces;
+
+interface ArgumentParserInterface
+{
+    public function parse(array $arguments, array $flags = [], array $params = []): array;
+}
