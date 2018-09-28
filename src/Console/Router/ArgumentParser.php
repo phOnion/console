5a3f578d3efef0efafa5c8240143427b1fc25871
Dimitar Dimitrov <daghostman.dd@gmail.com>
2018-09-28 21:29:34 +0300

    Argument parser now handles `|` alias arguments

--- src/Console/Router/ArgumentParser.php
+++ src/Console/Router/ArgumentParser.php
@@ -6,45 +6,62 @@ use Onion\Framework\Console\Interfaces\ArgumentParserInterface;
 
 class ArgumentParser implements ArgumentParserInterface
 {
-    public function parse(array $arguments, array $flags = [], array $parameters = []): array
+    public function parse(array &$arguments, array $parameters = []): array
     {
         $result = [];
-        for ($i=0, $count=count($arguments); $i<$count; $i++) {
-            $argument = $arguments[$i];
 
-            if (strpos($argument, '--') === 0) {
-                $name = substr($argument, 2);
-                if (strpos($name, '=')) {
-                    list($name,$value)=explode('=', $name, 2);
-                    if (in_array($name, $parameters, true)) {
-                        $result[$name] = $value;
-                    }
-                } else {
-                    if (isset($arguments[$i+1]) && strpos($arguments[$i+1], '-') !== 0) {
-                        $result[$name] = $arguments[++$i];
-                    } else {
-                        $result[$name] = true;
-                    }
+        foreach (array_keys($parameters) as $parameter) {
+            $aliases = array_map('trim', explode('|', $parameter));
+            foreach ($aliases as $alias) {
+                $i = array_search($alias, $arguments);
+                if ($i === false) {
+                    continue;
                 }
-            }
 
-            if (strpos($argument, '-') === 0) {
-                $name = substr($argument, 1, 1);
-                if (in_array($name, $flags, true)) {
-                    if (strlen($argument) > 2) {
-                        $result[$name] = substr($argument, 2);
+                $argument = $arguments[$i];
+                if (stripos($argument, "{$alias}=") === 0) {
+                    [$p, $value] = explode('=', $argument, 2);
+                    $result[$aliases[0]] = $value;
+                    unset($arguments[$index]);
+                    continue;
+                }
+
+                if ($argument === $alias) {
+                    $value = true;
+                    $type = $parameters[$alias]['type'] ?? '';
+                    unset($arguments[$i]);
+
+                    if (stripos($arguments[$i+1] ?? '-', '-') !== 0 && $type !== 'bool') {
+                        $value = $arguments[$i+1];
+                        unset($arguments[$i+1]);
                     }
 
-                    if (strlen($argument) === 2) {
-                        if (isset($arguments[$i+1]) && strpos($arguments[$i+1], '-') !== 0) {
-                            $result[$name] = $arguments[++$i];
-                        } else {
-                            $result[$name] = true;
-                        }
-                    }
+                    $result[$aliases[0]] = $value;
+                    continue;
                 }
             }
         }
+
+        foreach ($parameters as $parameter => $meta) {
+            if (!isset($meta['type'], $result[$parameter])) {
+                continue;
+            }
+
+            $checkFunction = "is_{$meta['type']}";
+            if (function_exists($checkFunction) && !$checkFunction($result[$parameter])) {
+                throw new \InvalidArgumentException(
+                    "The value '{$result[$parameter]}' for {$parameter} must be of type {$meta['type']}"
+                );
+            }
+
+            if (($meta['required'] ?? false) && !isset($result[$parameter])) {
+                throw new \BadFunctionCallException(
+                    "Missing required parameter '{$parameter}'"
+                );
+            }
+        }
+
+        $arguments = array_values($arguments);
 
         return $result;
     }
