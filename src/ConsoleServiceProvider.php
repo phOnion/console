86486463f830a56cda1a6c89b22dc858698dbbc6
Dimitar Dimitrov <daghostman.dd@gmail.com>
2022-11-30 21:39:17 +0200

    Add service provider that attempts to auto-register commands

--- src/ConsoleServiceProvider.php
+++ src/ConsoleServiceProvider.php
@@ -4,19 +4,46 @@ declare(strict_types=1);
 
 namespace Onion\Framework\Console;
 
+use Onion\Framework\Console\Annotations\Command;
+use Onion\Framework\Console\Annotations\Parameter;
 use Onion\Framework\Console\Interfaces\ArgumentParserInterface;
 use Onion\Framework\Console\Router\ArgumentParser;
 use Onion\Framework\Console\Router\Router;
 use Onion\Framework\Dependency\Interfaces\ContainerInterface;
-use Onion\Framework\Dependency\Interfaces\ServiceProviderInterface;
-use Onion\Framework\Router\Interfaces\RouterInterface;
 use Psr\Container\ContainerInterface as ContainerContainerInterface;
+use Onion\Framework\Dependency\Interfaces\BootableServiceProviderInterface;
+use ReflectionAttribute;
+use ReflectionObject;
 
-class ConsoleServiceProvider implements ServiceProviderInterface
+class ConsoleServiceProvider implements BootableServiceProviderInterface
 {
     public function register(ContainerInterface $provider): void
     {
         $provider->singleton(ArgumentParserInterface::class, new ArgumentParser());
-        $provider->bind(RouterInterface::class, fn (ContainerContainerInterface $c) => new Router($c->get(ArgumentParserInterface::class)));
+        $provider->bind(Router::class, fn (ContainerContainerInterface $c) => new Router($c->get(ArgumentParserInterface::class)));
+    }
+
+    public function boot(ContainerInterface $container): void
+    {
+        $container->extend(Router::class, function (Router $r, ContainerInterface $c) {
+            foreach ($c->tagged('command') as $command) {
+                $reflection = new ReflectionObject($command);
+                /** @var ?Command $cmd */
+                $cmd = current($reflection->getAttributes(Command::class, ReflectionAttribute::IS_INSTANCEOF))?->newInstance();
+                if (!$cmd) continue;
+
+                $r->addCommand($cmd->command, $command, [
+                    'summary' => $cmd->summary,
+                    'parameters' => array_map(fn (Parameter $p) => [
+                        'name' => $p->name . ($p->aliases ? ' | ' . implode(' | ' . $p->aliases) : ''),
+                        'description' => $p->description,
+                        'type' => $p->type,
+                        'default' => $p->default,
+                    ], array_map(fn (ReflectionAttribute $r) => $r->newInstance(), $reflection->getAttributes(Parameter::class, ReflectionAttribute::IS_INSTANCEOF))),
+                ]);
+            }
+
+            return $r;
+        });
     }
 }
